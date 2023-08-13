5cd050174a764e6055c13d614f4164aa09021998
Emma Souannhaphanh <77692774+etretf@users.noreply.github.com>
2023-08-13 12:35:24 -0400

    Setup React router dom

--- pomodoro-react/src/main.jsx
+++ pomodoro-react/src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
+import Home from './routes/Home'
+import ErrorPage from './routes/ErrorPage'
+import Progress from './routes/Progress'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <ErrorPage/>,
+  },
+  {
+    path: "/progress",
+    element: <Progress/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ pomodoro-react/src/routes/Progress.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Progress(){
+    return(
+        <>
+            <h1>My progress</h1>
+            <Link to="/">Home</Link>
+        </>
+    )
+}
