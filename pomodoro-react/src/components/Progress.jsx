4d29d8360e3834bf0f5d87f1abca372c2b8fa268
Emma Souannhaphanh <77692774+etretf@users.noreply.github.com>
2023-08-13 19:05:55 -0400

    Progress page

--- pomodoro-react/src/components/Progress.jsx
+++ pomodoro-react/src/components/Progress.jsx
@@ -5,21 +5,22 @@ import { useLocation } from "react-router-dom";
 
 export default function Progress(props){
     
+    const numTasks = [0,1,2,3]
 
     console.log(props);
     return(
-        <>
+        <div>
             {/* <h1>My progress</h1>
             <Link to="/">Home</Link>
             <button onClick={props.handleClick}>Click me now!</button> */}
-            <div className="border rounded-lg prose">
-                <h1 className="">My progress</h1>
+            <div className="border rounded-lg mx-auto max-w-full w-4/5 prose">
+                <h1 className="p-5">My progress</h1>
+                <canvas id="myProgressChart" style={{width:'700'}}></canvas>
             </div>
-            <div className="prose">
-            <h2 >Type 1</h2>
+            <div className="prose-lg">
+            <p>Type 1</p>
             </div>
 
-            
             <h2>Type 2</h2>
 
             <div >
@@ -30,6 +31,58 @@ export default function Progress(props){
                 <p className="prose-2xl">Small</p>
 
             </div>
-        </>
+            <div className="border rounded-lg mx-auto max-w-full w-4/5 prose">
+                <h1 className="p-5">Completed Tasks</h1>
+                { numTasks.map(task => <Task/>)}
+            </div>
+        </div>
+    )
+}
+
+//  this will be changed when fetching from API
+function getDate(daysInPast){
+    let date = new Date();
+    date.setDate(today.getDate()-daysInPast);
+    return date;
+}
+let today = new Date();
+today.setDate(today.getDate()-1);
+today.toLocaleDateString('en-us', {weekday:'long'});
+console.log(today);
+// const event = new Date().toLocaleDateString('en-us', {weekday:'long'});
+// console.log(event);
+// for(let i = 0; i < 6; i++){
+//     let weekday = 
+// }
+
+const daysOfTheWeek = [];
+
+const myProgressChart = new Chart('myProgressChart', {
+    type:'bar',
+    data:{
+        labels: daysOfTheWeek,
+    },
+    options:{},
+})
+
+
+function Task(){
+    return(
+        <div className="flex border p-3 mb-3 mx-6 rounded max-w-full justify-between prose">
+            <div className="">
+                <h3 className="m-0">
+                    BIT2002
+                </h3>
+                <p>
+                    Take notes on chapter 10 and 11.
+                </p>                      
+            </div>
+            <div className="flex items-center">
+                <p>
+                    Completed at 19:23, October 10, 2022.
+                </p>
+            </div>
+
+        </div>
     )
 }
