ce20b86e856e763f596f7817acb691735ee4ab46
acce8711 <101413675+acce8711@users.noreply.github.com>
2023-08-15 22:28:57 -0400

    worked a bit on to do ui

--- pomodoro-react/src/components/ToDo.jsx
+++ pomodoro-react/src/components/ToDo.jsx
@@ -1,10 +1,16 @@
 import ToDoItem from "./ToDoItem"
 import ToDoItemTemplate from "./ToDoItemTemplate"
+import { PlusIcon } from '@heroicons/react/24/solid'
 
 export default function ToDo()
 {
     return(
-        <div className="full-w-component half-w-component to-do-component">
+        <div className="full-w-component half-w-component to-do-component ">
+            <div className="x-flex w-full">
+                <h2>My Tasks</h2>
+                <button className="btn btn-outline"><PlusIcon className="h-4 w-4 text-blue-500" /> 
+                Add task</button>
+            </div>
                 <ToDoItem />
                 <ToDoItem />
                 
