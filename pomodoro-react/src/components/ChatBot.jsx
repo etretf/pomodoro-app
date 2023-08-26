828e463dc9edf9f3689916d0517894b5daf939d0
acce8711 <101413675+acce8711@users.noreply.github.com>
2023-08-26 15:03:44 -0400

    udpated chat

--- pomodoro-react/src/components/ChatBot.jsx
+++ pomodoro-react/src/components/ChatBot.jsx
@@ -14,19 +14,18 @@ const openai = new OpenAI(
 
 export default function ChatBot()
 {
-    const [chat, setChat] = useState([
-        {role: "user", content: "hi"},
-        {role: "assistant", content: "bye"}
-    ]);
+    const [chat, setChat] = useState([]);
     const [chatQuestion, setChatQuestion] = useState("")
+    const [questionToDisplay, setQuestionToDisplay] = useState("")
     const [generatingText, setGenerating] = useState(false)
 
     
     const handleSubmit = async (event, message) => {
         event.preventDefault()
         setChatQuestion("")
+        setQuestionToDisplay(message);
         setGenerating(true)
-        setChat(prevValue => ([...prevValue, {role: "user", content: message}]))
+        let contentToAdd = {question: message,content: ""}
         await openai.chat.completions.create(
             {
                 model: "gpt-3.5-turbo",
@@ -37,7 +36,8 @@ export default function ChatBot()
                   }],
               }
             ).then((result) => {
-              setChat(prevValue => ([...prevValue, result.choices[0].message]))
+              contentToAdd.content = result.choices[0].message.content;
+              setChat(prevValue => ([...prevValue, contentToAdd]))
               setGenerating(false)
               console.log(chat)
             }).catch(error => console.log(error))
@@ -57,17 +57,12 @@ export default function ChatBot()
         chatItem={item} />
     ))
 
+    currentChat.reverse()
+
     return(
-        <div className="full-w-component half-w-component to-do-component relative">
-            <ChatBubble chatItem={{role: "assistant", content: "Hi! I'm Pomodoro, a tomato!"}}/>
-            {currentChat}
-            {generatingText && <ChatBubble chatItem={{role: "assistant", content: "Typing..."}}/>}
-            <button 
-            className="btn btn-xs btn-ghost  absolute right-2 top-2" 
-            onClick={clearChat}
-            disabled={!chat.length}
-            >Clear Chat</button>
-            <form className="x-flex w-full mt-auto" onSubmit={event => handleSubmit(event, chatQuestion)}> 
+        <div className="full-w-component half-w-component to-do-component">
+            
+            <form className="x-flex w-full" onSubmit={event => handleSubmit(event, chatQuestion)}> 
                 <input 
                 type="text" 
                 placeholder="What would you like to know?" 
@@ -76,6 +71,14 @@ export default function ChatBot()
                 onChange={handleChange}/>
                 <button className='btn btn-sm btn-primary'>Go</button>
             </form>
+            <button 
+            className="btn btn-xs btn-ghost mr-auto" 
+            onClick={clearChat}
+            disabled={!chat.length}
+            >Clear Chat</button>
+            {generatingText && <ChatBubble chatItem={{question: questionToDisplay, content: "Typing..."}}/>}
+            {currentChat}
+            <ChatBubble chatItem={{question:"", content: "Hi! I'm Pomodoro, a tomato!"}}/>
         </div>
     )
 }
--- pomodoro-react/src/components/ChatBubble.jsx
+++ pomodoro-react/src/components/ChatBubble.jsx
@@ -3,16 +3,13 @@
 export default function ChatBubble(props)
 {
     return (
-        <div className={`chat w-full ${props.chatItem.role == "user" ? "chat-end" : "chat-start"}`}>
-                <div className="chat-header">{props.chatItem.role == "user"? "user" : "pomodoro"}</div>
-                {props.chatItem.role != "user"
-                &&
+        <div className="chat w-full chat-start">
+                <div className="chat-header">{props.chatItem.question}</div>
                 <div className="chat-image avatar">
                     <div className="w-10 rounded-full">
                     <img src="https://static.vecteezy.com/system/resources/previews/010/672/228/original/cute-funny-tomato-sticker-character-hand-drawn-cartoon-kawaii-character-illustration-icon-isolated-on-green-background-tomato-character-concept-vector.jpg" />
                     </div>
                 </div>
-                }
                 
                 <div className="chat-bubble bg-base-100">{props.chatItem.content}</div>
         </div>
